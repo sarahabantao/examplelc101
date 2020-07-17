d2942f53f7345e2f328faf17021ec020cc8f3ca1
Sarah <abantaos@hotmail.com>
2020-07-16 20:44:11 -0500

    example css

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  window.addEventListener("load", function() {
+    var x = document.getElementById("redp");
+    x.innerHTML = "Lashley";
+    x.style.fontSize = "48px"; 
+    x.style.color = "#c52551"; 
+    } );
